d062fad1a6b74a34f4f1b5e5cd02b9d1c40806ce
Soumik0411 <soumikseal6@gmail.com>
2022-01-10 18:25:10 +0530

    added routes,apis and database

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,58 @@
-import React from 'react'
+import {React, useState} from 'react';
+import { Navigate, useNavigate } from 'react-router';
+import axios from 'axios';
 
 export default function Login() {
+  const [user, setuser] = useState();
+  const navigate = useNavigate();
+  const handleCheck=(e)=>{
+    var n=String(user)
+    console.log(n.split('@')[1])
+    var d=n.split('@')[1];
+    // axios.post("http://localhost:5000/get_participant/", {
+    //     Domain: user
+    //   }).then(response => {
+    //     console.log('response >>> ', response);
+    //     if(response.data.status==="success")
+    //     {
+    //       axios.post("http://localhost:5000/get_domain/", {
+    //         Domain:d
+    //       }).then(response => {
+    //         console.log('response >>> ', response);
+    
+    //       }).catch(error => {
+    //         console.error('error >>> ', error);
+    //       });
+    
+    //     }
+
+    //   }).catch(error => {
+    //     console.error('error >>> ', error);
+    //   });
+    axios.post("http://localhost:5000/get_domain/", {
+      Domain: d
+    }).then(response => {
+      console.log('response >>> ', response);
+      if(response.data.data[0].orgName==="Adamas University")
+      {
+        navigate('/adamasuniversity');
+      }
+      else{
+        navigate('/mit');
+      }
+
+    }).catch(error => {
+      console.error('error >>> ', error);
+    });
+
+  }
   return (
     <div className='Log'>
         <img src="testi2.png" alt="user" className='user-img'/>
         <br/>
-      <input type="text" placeholder='Username' className='user'/>
+      <input type="text" value={user} placeholder='Username' className='user'onChange={e => [setuser(e.target.value)]}/>
       <br/>
-      <button type="submit" className='btn'>Check</button>
+      <button type="submit" className='btn' onClick={handleCheck}>Check</button>
     </div>
   )
 }
